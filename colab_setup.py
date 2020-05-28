72244a8e6f6244ed0cf799e6f1a4bcc62fee5de7
CHAN,KELVIN <kechan@gmail.com>
2020-05-28 02:52:23 -0400

    update colab_setup.py

--- colab_setup.py
+++ colab_setup.py
@@ -4,31 +4,78 @@ from datetime import date, datetime, timedelta
 from IPython.display import HTML
 from io import BytesIO
 
-from google.colab import auth
-auth.authenticate_user()
-print('Authenticated')
+def onColab(): return os.path.exists('/content')
+bOnColab = onColab()
+
+if bOnColab:
+  from google.colab import auth
+  auth.authenticate_user()
+  print('Authenticated')
   
-from google.colab import drive
-drive.mount('/content/drive')
-print('gdrive mounted')
+if bOnColab and not os.path.exists('/content/drive'):       # presence of /content indicates you are on google colab
+  from google.colab import drive
+  drive.mount('/content/drive')
+  print('gdrive mounted')
+
+try:
+  !pip install tf-nightly --quiet
+except:
+  pass
 
 import pandas as pd
 import numpy as np
 
 from google.cloud import storage
 
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+from tensorflow.keras.utils import Sequence
+
+from tensorflow.keras.layers import Dense, Input, Embedding, LSTM, Reshape, Dropout, Activation, Dot
+from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras import backend as K
+
+from tensorflow.keras.layers.experimental.preprocessing import RandomFlip, RandomTranslation, RandomRotation
+from tensorflow.keras.layers.experimental.preprocessing import RandomCrop, RandomContrast
+
 from ipywidgets import interact, Checkbox, Button, Output, HBox, VBox, AppLayout, Label, Layout, Text, Textarea
 from ipywidgets import HTML as IPyWidgetHTML     # conflict with "from IPython.display import HTML"
 
+from IPython.display import Image, Audio
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib import style
+import matplotlib.dates as mdates
+import seaborn as sns
+# sns.set(style='darkgrid', context='talk', palette='Dark2')
+sns.set(rc={'figure.figsize': (11, 4)})
+
+plt.rcParams['figure.figsize'] = (12, 6)
+plt.rcParams['axes.linewidth'] = 2
+plt.rcParams['legend.fontsize'] = 14
+plt.rcParams['font.size'] = 16
+
+bJumptools = False
+
 project_name = 'ListingImageClassification'
 
-home = Path('/content/drive/My Drive')
-data = home/project_name/'data'
-train_home = home/project_name/'training'
-photos = home/project_name/'photos'
-tmp = home/project_name/'tmp'
-labels_dir = home/project_name/'labels'
-utils_path = home/project_name/'utils'
+if bOnColab:
+  home = Path('/content/drive/My Drive')
+  if (home/'jumptools.com').exists(): bJumptools = True
+  if bJumptools:
+    data = home/project_name/'data'
+    train_home = home/project_name/'training'
+    photos = home/project_name/'photos'
+    tmp = home/project_name/'tmp'
+    labels_dir = home/project_name/'labels'
+    utils_path = home/project_name/'utils'
+  else:
+    data = home/'rlp'/project_name/'data'
+    train_home = home/'rlp'/project_name/'training'
+    utils_path = home/'rlp'/project_name/'utils'
 
 sys.path.insert(0, str(utils_path))
 
@@ -40,3 +87,44 @@ from small_fastai_utils import join_df
 
 from common_util import ImageDataLoader, say
 
+print("\u2022 Using TensorFlow Version:", tf.__version__)
+
+# This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
+def plotImages(images_arr):
+  fig, axes = plt.subplots(1, 5, figsize=(20,20))
+  axes = axes.flatten()
+  for img, ax in zip( images_arr, axes):
+      ax.imshow(img)
+      ax.axis('off')
+  plt.tight_layout()
+  plt.show()
+
+# For inline image visualization  
+
+pd.set_option('display.max_colwidth', -1)
+listingid_regex = re.compile(r'.*/(\d+)_\d+\.jpg')
+
+def get_listingId(row):
+  idx = row.name
+  return listingid_regex.match(str(dev_idx_filenames[idx])).group(1)
+
+def get_thumbnail(row):
+  idx = row.name
+  
+  img = PIL.Image.fromarray((test_x[idx]*255).astype('uint8'), 'RGB')
+  img.thumbnail((150, 150), PIL.Image.LANCZOS)
+
+  return img
+
+def image_base64(im):
+  if isinstance(im, str):
+      im = get_thumbnail(im)
+  with BytesIO() as buffer:
+      im.save(buffer, 'jpeg')
+      return base64.b64encode(buffer.getvalue()).decode()
+
+def image_formatter(im):
+  return f'<img src="data:image/jpeg;base64,{image_base64(im)}">'
+
+def sigmoid(x):
+  return 1./(1. + np.exp(-x))
